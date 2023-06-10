9aee7ab2ec4e614f877d5b625a2a9cf05758500b
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-10 12:54:39 +0200

    Update calculateIndex: add parentheses

--- src/main/java/pl/coderslab/driver/util/IndexCalculator.java
+++ src/main/java/pl/coderslab/driver/util/IndexCalculator.java
@@ -2,6 +2,6 @@ package pl.coderslab.driver.util;
 
 public class IndexCalculator {
   public long calculateIndex(int weekNumber, int year, long numberOfAdvices){
-    return weekNumber > numberOfAdvices ? ((weekNumber % numberOfAdvices)+1) : (weekNumber * (year % (numberOfAdvices/weekNumber)+1));
+    return weekNumber > numberOfAdvices ? ((weekNumber % numberOfAdvices)+1) : (weekNumber * ((year % (numberOfAdvices/weekNumber))+1));
   }
 }
