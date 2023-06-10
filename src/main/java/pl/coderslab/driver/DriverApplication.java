4b9486f498d10d72f222cd8f02c1e7c05dddeb93
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-10 13:06:23 +0200

    Add bean calculator()

--- src/main/java/pl/coderslab/driver/DriverApplication.java
+++ src/main/java/pl/coderslab/driver/DriverApplication.java
@@ -2,9 +2,16 @@ package pl.coderslab.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.coderslab.driver.util.IndexCalculator;
 
 @SpringBootApplication
 public class DriverApplication {
+
+	@Bean
+	public IndexCalculator calculator() {
+		return new IndexCalculator();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DriverApplication.class, args);
