47c455818903f674bad210454a68012eeadd0749
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-12 00:43:57 +0200

    Update import: add user Admin with password Admin

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,3 +15,5 @@ insert into driver.trainings (advice_id) values (1), (2);
 update driver.questions set training_id = 1 where id between 1 and 3;
 
 update driver.questions set training_id = 2 where id between 4 and 6;
+
+insert into driver.users (user_name, password, active) values ('Admin', '$2a$10$H8betvRlG91b.P0INAsfnufN4Wc2WClZN9VvR2XpqvlXbRLTnWLpW', true);
