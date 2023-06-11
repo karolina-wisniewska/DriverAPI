0fe12227d7c830beca9fbb0d40c5c42557eb6337
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-12 00:43:05 +0200

    Add configuration annotation

--- src/main/java/pl/coderslab/driver/DriverApplication.java
+++ src/main/java/pl/coderslab/driver/DriverApplication.java
@@ -2,10 +2,13 @@ package pl.coderslab.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import pl.coderslab.driver.config.RsaKeyProperties;
 import pl.coderslab.driver.util.IndexCalculator;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class DriverApplication {
 
 	@Bean
