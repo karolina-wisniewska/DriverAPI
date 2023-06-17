884ddb7042e3f786dd32f9985ad12a48ef6af73d
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-17 21:06:18 +0200

    Update MediaContentController, ResponseFile

--- src/main/java/pl/coderslab/driver/controller/MediaContentController.java
+++ src/main/java/pl/coderslab/driver/controller/MediaContentController.java
@@ -5,14 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.coderslab.driver.entity.MediaContent;
 import pl.coderslab.driver.message.ResponseFile;
 import pl.coderslab.driver.message.ResponseMessage;
@@ -29,7 +30,7 @@ public class MediaContentController {
 
   private final MediaContentService mediaContentService;
 
-  @PostMapping(value = "/upload", consumes = {
+  @PostMapping(consumes = {
           "multipart/form-data"})
   public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
     String message = "";
@@ -44,31 +45,29 @@ public class MediaContentController {
     }
   }
 
-  @GetMapping("/files")
+  @GetMapping
   public ResponseEntity<List<ResponseFile>> getListFiles() {
-    List<ResponseFile> files = mediaContentService.findAll().map(dbFile -> {
-      String fileDownloadUri = ServletUriComponentsBuilder
-              .fromCurrentContextPath()
-              .path("/files/")
-              .path(String.valueOf(dbFile.getId()))
-              .toUriString();
-
-      return new ResponseFile(
-              dbFile.getName(),
-              fileDownloadUri,
-              dbFile.getType(),
-              dbFile.getData().length);
-    }).collect(Collectors.toList());
+    List<ResponseFile> files = mediaContentService.findAll().map(dbFile -> new ResponseFile(
+            dbFile.getName(),
+            dbFile.getType(),
+            dbFile.getData().length)).collect(Collectors.toList());
 
     return ResponseEntity.status(HttpStatus.OK).body(files);
   }
 
-  @GetMapping("/files/{id}")
-  public ResponseEntity<byte[]> getFile(@PathVariable Long id) {
-    MediaContent fileDB = mediaContentService.findById(id);
+  @GetMapping("/{mediaId}")
+  public ResponseEntity<byte[]> getFile(@PathVariable Long mediaId) {
+    MediaContent fileDB = mediaContentService.findById(mediaId);
 
     return ResponseEntity.ok()
             .contentType(MediaType.valueOf(fileDB.getType()))
             .body(fileDB.getData());
   }
+
+  @DeleteMapping("/{mediaId}")
+  @ResponseStatus(HttpStatus.OK)
+  public void deleteMedia(@PathVariable long mediaId) {
+    mediaContentService.deleteById(mediaId);
+  }
+
 }
