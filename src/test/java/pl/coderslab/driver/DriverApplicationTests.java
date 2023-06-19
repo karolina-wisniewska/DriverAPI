a6db134bc05a0ed7f64bf850d99ad48d0fa9535d
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-19 09:46:03 +0200

    Update: add annotations

--- src/test/java/pl/coderslab/driver/DriverApplicationTests.java
+++ src/test/java/pl/coderslab/driver/DriverApplicationTests.java
@@ -1,9 +1,13 @@
 package pl.coderslab.driver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+				webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+				classes = DriverApplication.class)
+@AutoConfigureMockMvc
 class DriverApplicationTests {
 
 	@Test
