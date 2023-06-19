fcb79401ad401b16af95452fca47152489d18f0b
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-06-19 11:38:35 +0200

    Add annotations

--- src/main/java/pl/coderslab/driver/model/TagDto.java
+++ src/main/java/pl/coderslab/driver/model/TagDto.java
@@ -1,8 +1,14 @@
 package pl.coderslab.driver.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TagDto {
 
   private Long id;
