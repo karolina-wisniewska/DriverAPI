d82d29648b8f1cddcfbc42aca82b4832ed91b731
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-05 13:19:19 +0200

    Add AuthenticationManager bean

--- src/main/java/pl/coderslab/driver/config/WebSecurityConfiguration.java
+++ src/main/java/pl/coderslab/driver/config/WebSecurityConfiguration.java
@@ -11,7 +11,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -45,6 +47,11 @@ public class WebSecurityConfiguration {
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
   }
 
   @Bean
