ac108390e0108d4b867e3392518b40ce50ff3044
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-05 13:23:49 +0200

    Update method not requiring authentication

--- src/main/java/pl/coderslab/driver/controller/HelloController.java
+++ src/main/java/pl/coderslab/driver/controller/HelloController.java
@@ -1,18 +1,14 @@
 package pl.coderslab.driver.controller;
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
-@SecurityRequirement(name = "driver-api")
 public class HelloController {
 
   @GetMapping("/hello")
-  public String hello(Principal principal) {
-    return "Hello, " + principal.getName();
+  public String hello() {
+    return "Hello from Driver API";
   }
 
 }
