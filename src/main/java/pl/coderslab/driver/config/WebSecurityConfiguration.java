2adc69551931a461d4a90527d662847460fec712
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-06 21:03:34 +0200

    Update add EnableMethodSecurity annotation

--- src/main/java/pl/coderslab/driver/config/WebSecurityConfiguration.java
+++ src/main/java/pl/coderslab/driver/config/WebSecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 @OpenAPIDefinition(info = @Info(title = "Driver API", version = "1.0", description = "API for advices and trainings for drivers"))
 public class WebSecurityConfiguration {
