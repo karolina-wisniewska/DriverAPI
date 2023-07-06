5936feec71e7ef5f16cfe49338174ad16ec375a7
karolina-wisniewska <karolina.raczkowska.94@gmail.com>
2023-07-06 21:05:16 +0200

    Update move authentication to controller

--- src/main/java/pl/coderslab/driver/service/security/TokenService.java
+++ src/main/java/pl/coderslab/driver/service/security/TokenService.java
@@ -1,16 +1,12 @@
 package pl.coderslab.driver.service.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
-import pl.coderslab.driver.pojo.AuthenticationRequest;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -21,29 +17,17 @@ import java.util.stream.Collectors;
 public class TokenService {
 
   private final JwtEncoder encoder;
-  private final AuthenticationManager authenticationManager;
-  private final UserDetailsService userDetailsService;
 
-  public String generateToken(AuthenticationRequest request) {
-
-    authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                    request.getUserName(),
-                    request.getPassword()
-            )
-    );
-
-    UserDetails userDetails = userDetailsService.loadUserByUsername(request.getUserName());
-
+  public String generateToken(Authentication authentication) {
     Instant now = Instant.now();
-    String scope = userDetails.getAuthorities().stream()
+    String scope = authentication.getAuthorities().stream()
             .map(GrantedAuthority::getAuthority)
             .collect(Collectors.joining(" "));
     JwtClaimsSet claims = JwtClaimsSet.builder()
             .issuer("self")
             .issuedAt(now)
             .expiresAt(now.plus(1, ChronoUnit.HOURS))
-            .subject(request.getUserName())
+            .subject(authentication.getName())
             .claim("scope", scope)
             .build();
     return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
